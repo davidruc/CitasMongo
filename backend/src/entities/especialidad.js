ea81b94b9910fe2b3a17c35327c5b842f2d82d9a
davidruc <davidarueda18@gmail.com>
2023-08-23 07:02:22 -0500

    feat: :sparkles: crud for the collection especialidad

---
+++ backend/src/entities/especialidad.js
@@ -0,0 +1,70 @@
+import { conexion } from "../../db/atlas.js";
+
+class Especialidad{    
+    _id;
+    esp_id;
+    esp_nombre;
+    constructor(){};
+    async connect(){
+        try {
+            const result = await conexion("especialidad"); 
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getAllEspecialidades(){
+        try {
+            const connection = await this.connect();
+            const result = await connection.find({}).toArray();
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getEspecialidadesByID(id){
+        try {
+            const connection = await this.connect();
+            const result = await connection.aggregate([
+                { $match: { "acu_codigo": parseInt(id)}}]).toArray();
+                console.log(result);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async postEspecialidad(data){
+        try {
+            const connection = await this.connect();
+            const result = await connection.insertOne(data);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async updateEspecialidad(id, data){
+        try {
+            const connection = await this.connect();
+            const result = await connection.updateOne(
+                { "esp_id": parseInt(id)},
+                { $set: data }
+            );
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async deleteEspecialidad(id){
+        try {
+            const connection = await this.connect();
+            const result = await connection.deleteOne(
+                {"acu_codigo": parseInt(id)}
+            );
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export {Especialidad}
